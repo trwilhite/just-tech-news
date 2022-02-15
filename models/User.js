8c455b688398f0a3bdaf3333ff1900fac3602383
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-02-14 22:52:33 -0500

    create login route

--- models/User.js
+++ models/User.js
@@ -4,8 +4,12 @@ const bcrypt = require('bcrypt');
 
 
 // create our User model
-class User extends Model { }
-
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 // define table columns and configuration
 User.init(
     {
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -27,7 +27,14 @@ router.get('/:id', (req, res) => {
                 res.status(404).json({ message: 'No user found with this id' });
                 return;
             }
-            res.json(dbUserData);
+            // res.json(dbUserData);
+            const validPassword = dbUserData.checkPassword(req.body.password);
+            if (!validPassword) {
+                res.status(400).json({ message: 'Incorrect password!' });
+                return;
+            }
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
         })
         .catch(err => {
             console.log(err);
@@ -48,6 +55,25 @@ router.post('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        res.json({ user: dbUserData });
+
+        // Verify user
+
+    });
 });
 
 // PUT /api/users/1
